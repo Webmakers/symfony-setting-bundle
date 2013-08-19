186fcfd290e417668b1a32d480be7b056a47a319
Khaled Attia <github@khal3d.com>
2013-08-19 05:26:14 +0200

    Added WebmakersSettingBundle

---
+++ Entity/Setting.php
@@ -0,0 +1,153 @@
+<?php
+
+namespace Webmakers\SettingBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Setting
+ *
+ * @ORM\Table(name="settings")
+ * @ORM\Entity(repositoryClass="Webmakers\SettingBundle\Entity\SettingRepository")
+ */
+class Setting
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="label", type="string", length=255)
+     */
+    private $label;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="value", type="text")
+     */
+    private $value;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="type", type="string", length=50)
+     */
+    private $type;
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set label
+     *
+     * @param string $label
+     * @return Setting
+     */
+    public function setLabel($label)
+    {
+        $this->label = $label;
+
+        return $this;
+    }
+
+    /**
+     * Get label
+     *
+     * @return string
+     */
+    public function getLabel()
+    {
+        return $this->label;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Setting
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set value
+     *
+     * @param string $value
+     * @return Setting
+     */
+    public function setValue($value)
+    {
+        $this->value = $value;
+
+        return $this;
+    }
+
+    /**
+     * Get value
+     *
+     * @return string
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+
+    /**
+     * Set type
+     *
+     * @param string $type
+     * @return Setting
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * Get type
+     *
+     * @return string
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+}
---
+++ WebmakersSettingBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Webmakers\SettingBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class WebmakersSettingBundle extends Bundle
+{
+}
